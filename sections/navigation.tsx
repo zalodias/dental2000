0690d3679d533c708dfe0f853fbb3c7524b7e472
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-09-21 16:52:49 +0100

    ✨ Implement responsive Navigation component with menu toggle

---
+++ sections/navigation.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { Button } from '@/components/button';
+import { navigation } from '@/data/navigation';
+import useScrollDirection from '@/hooks/useScrollDirection';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const scrollDirection = useScrollDirection();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header>
+      <nav
+        className={`fixed inset-x-0 top-0 z-20 flex items-center justify-between px-10 py-4 transition-all duration-400 ${
+          scrollDirection === 'down' ? '-translate-y-full' : 'translate-y-0'
+        } ${isMenuOpen ? 'bg-background-neutral-default border-b border-transparent' : 'bg-background-neutral-default border-border-neutral-subtle border-b'}`}
+      >
+        <Link
+          href="/"
+          className="text-title-small font-semibold tracking-tight"
+        >
+          Dental 2000
+        </Link>
+        <div className="flex items-center gap-10">
+          <div className="hidden md:flex md:gap-10">
+            <ul className="hidden items-center gap-10 md:flex">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <Link href={item.href} className="whitespace-nowrap">
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Button href="/marcar-consulta">Marcar consulta</Button>
+          </div>
+          <button
+            onClick={toggleMenu}
+            className="grid cursor-pointer gap-2 md:hidden"
+          >
+            <span
+              className={`bg-foreground-neutral-default h-px w-5 transition-transform duration-200 ${isMenuOpen ? 'translate-y-1 rotate-45' : ''}`}
+            ></span>
+            <span
+              className={`bg-foreground-neutral-default h-px w-5 transition-transform duration-200 ${isMenuOpen ? '-translate-y-1 -rotate-45' : ''}`}
+            ></span>
+          </button>
+        </div>
+      </nav>
+      <div
+        className={`bg-background-neutral-default fixed inset-0 z-10 flex h-screen flex-col items-center justify-center gap-10 transition-opacity duration-400 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}
+        inert={!isMenuOpen}
+      >
+        <ul className="flex flex-col items-start gap-10 md:hidden">
+          {navigation.map((item) => (
+            <li key={item.name}>
+              <Link
+                href={item.href}
+                className="text-display-small tracking-tight whitespace-nowrap"
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
+}
