3c1b337979c66421df30e09b57123db62b517d17
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-09-21 11:27:53 +0100

    ✨ Add Container component

---
+++ components/container.tsx
@@ -0,0 +1,19 @@
+import { mergeTailwindClassNames as cn } from '@/utils/tailwind';
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Container({ children, className }: ContainerProps) {
+  return (
+    <main
+      className={cn(
+        'mx-auto flex w-full max-w-(--breakpoint-xl) flex-col gap-20 px-5 pt-10 md:px-10',
+        className,
+      )}
+    >
+      {children}
+    </main>
+  );
+}
