e7baf906e3b180aad40a75cda9034e7b054edcb8
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-09-23 22:42:52 +0100

    ✨ Improve Container layout styling

--- components/container.tsx
+++ components/container.tsx
@@ -9,7 +9,7 @@ export function Container({ children, className }: ContainerProps) {
   return (
     <div
       className={cn(
-        'mx-auto w-full max-w-(--breakpoint-xl) gap-20 px-5 py-10 md:px-10 md:py-20 lg:px-20 lg:py-30',
+        'mx-auto flex w-full max-w-(--breakpoint-xl) flex-col gap-10 px-5 py-10 md:gap-15 md:px-10 md:py-20 lg:px-20 lg:py-30',
         className,
       )}
     >
