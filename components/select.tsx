a85badc69767eb16bfa88ad18e28c9a7171df5b8
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-09-23 22:42:32 +0100

    ✨ Add Select component

---
+++ components/select.tsx
@@ -0,0 +1,12 @@
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+
+export function Select({ className, children, ...props }: SelectProps) {
+  return (
+    <select
+      className="border-border-neutral-default focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      {...props}
+    >
+      {children}
+    </select>
+  );
+}
