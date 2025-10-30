78be242d41d261ec8ddebf1e4b2387469c37c7ae
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-10-30 08:30:05 +0000

    ✨ Add alignment property to SectionHeader

--- sections/section-header.tsx
+++ sections/section-header.tsx
@@ -3,6 +3,7 @@ interface SectionHeaderProps {
   title: string;
   subtitle?: string;
   action?: React.ReactNode;
+  alignment?: 'start' | 'center';
 }
 
 export function SectionHeader({
@@ -10,17 +11,32 @@ export function SectionHeader({
   title,
   subtitle,
   action,
+  alignment = 'start',
 }: SectionHeaderProps) {
   return (
-    <div className="flex flex-col gap-10">
-      <div className="flex flex-col gap-5">
+    <div
+      className={`flex max-w-[80ch] flex-col gap-10 ${
+        alignment === 'center' && 'items-center text-center'
+      }`}
+    >
+      <div
+        className={`flex flex-col gap-5 ${
+          alignment === 'center' && 'items-center text-center'
+        }`}
+      >
         {eyebrow && (
           <h3 className="text-body-medium font-medium tracking-widest uppercase">
             {eyebrow}
           </h3>
         )}
-        <div className="flex flex-col gap-5">
-          <h2 className="text-display-medium font-medium">{title}</h2>
+        <div
+          className={`flex flex-col gap-5 ${
+            alignment === 'center' && 'items-center text-center'
+          }`}
+        >
+          <h2 className="text-display-medium font-medium text-balance">
+            {title}
+          </h2>
           {subtitle && (
             <p className="text-foreground-neutral-subtle text-title-small">
               {subtitle}
