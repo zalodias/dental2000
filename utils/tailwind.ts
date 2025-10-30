fd73c4b981ed9f2fb540f3316e8d9f8280c265a4
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-10-30 08:05:28 +0000

    ✨ Extend Tailwind merge to prevent text class conflicts

--- utils/tailwind.ts
+++ utils/tailwind.ts
@@ -1,5 +1,23 @@
 import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        'text-display-large',
+        'text-display-medium',
+        'text-display-small',
+        'text-title-large',
+        'text-title-medium',
+        'text-title-small',
+        'text-body-large',
+        'text-body-medium',
+        'text-body-small',
+      ],
+    },
+  },
+});
 
 export function mergeTailwindClassNames(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
