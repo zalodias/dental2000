67ab032618c6dc10133de87efa91aba1161fa872
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-01 12:32:36 +0000

    ✨ Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,22 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <Container className="grid place-content-center">
-      <Image src={Logo} alt="Dental 2000" width={240} />
-    </Container>
+    <>
+      <section className="relative grid place-items-center">
+        <img
+          src="#"
+          alt="Image"
+          className="bg-background-neutral-subtle -z-10 aspect-2/3 w-full object-cover sm:aspect-1/1 md:aspect-3/2 lg:aspect-2/1"
+        />
+        <Container className="absolute gap-5 md:gap-10">
+          <h1 className="text-display-medium md:text-display-large font-medium">
+            O seu sorriso, a nossa prioridade.
+          </h1>
+          <Button>
+            <Link href="#">Marcar consulta</Link>
+          </Button>
+        </Container>
+      </section>
+    </>
   );
 }
