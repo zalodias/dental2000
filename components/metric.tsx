3e3e54ea19ed5b8f66207cb3dbb67de24edd46f5
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-01 23:39:06 +0000

    ✨ Add external class support for Metric component

--- components/metric.tsx
+++ components/metric.tsx
@@ -1,11 +1,14 @@
+import { mergeTailwindClassNames as cn } from '@/utils/tailwind';
+
 interface MetricProps {
   label: string;
   value: string | number;
+  className?: string;
 }
 
-export function Metric({ label, value }: MetricProps) {
+export function Metric({ label, value, className }: MetricProps) {
   return (
-    <div className="flex flex-col gap-1">
+    <div className={cn('flex flex-col gap-1', className)}>
       <div className="text-display-medium font-medium">{value}</div>
       <div className="text-title-small text-foreground-neutral-subtle">
         {label}
