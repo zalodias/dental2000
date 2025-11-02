945c3465c0001dcfbaabe69f76e26661d8219822
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-02 08:27:31 +0000

    ✨ Enhance Textarea with dynamic height resize

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,10 +1,16 @@
+'use client';
+
 interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 export function Textarea({ className, ...props }: TextareaProps) {
   return (
     <textarea
-      className="border-border-neutral-default focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      className="border-border-neutral-default placeholder:text-foreground-neutral-faded text-title-small focus:border-border-neutral-strong [field-sizing:content] overflow-clip border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      onInput={(e) => {
+        e.currentTarget.style.height = '';
+        e.currentTarget.style.height = e.currentTarget.scrollHeight + 'px';
+      }}
       {...props}
     />
   );
