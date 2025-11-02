07df73470073242137324a27ff192141148df8bb
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-02 08:33:02 +0000

    🎨 Replace hero image with placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,7 @@ export default function Home() {
   return (
     <>
       <section className="relative grid place-items-center">
-        <img
-          src="#"
-          alt="Image"
-          className="bg-background-neutral-subtle -z-10 aspect-2/3 w-full object-cover sm:aspect-1/1 md:aspect-3/2 lg:aspect-2/1"
-        />
+        <div className="bg-background-neutral-subtle -z-10 aspect-2/3 w-full object-cover sm:aspect-1/1 md:aspect-3/2 lg:aspect-2/1" />
         <Container className="absolute gap-5 md:gap-10">
           <h1 className="text-display-medium md:text-display-large font-medium">
             O seu sorriso, a nossa prioridade.
