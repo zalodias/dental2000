60ee77321ad510acc4d455181c85659acdf20592
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-02 08:28:32 +0000

    🎨 Update Input and Select text size

--- components/input.tsx
+++ components/input.tsx
@@ -3,7 +3,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 export function Input({ className, ...props }: InputProps) {
   return (
     <input
-      className="border-border-neutral-default focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      className="border-border-neutral-default text-title-small placeholder:text-foreground-neutral-faded focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
       {...props}
     />
   );
