e971b952cf1333e0228f9ef1c743e0fd00ee29dc
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-02 08:28:32 +0000

    🎨 Update Input and Select text size

--- components/select.tsx
+++ components/select.tsx
@@ -3,7 +3,7 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
 export function Select({ className, children, ...props }: SelectProps) {
   return (
     <select
-      className="border-border-neutral-default focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      className="border-border-neutral-default text-title-small focus:border-border-neutral-strong border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
       {...props}
     >
       {children}
