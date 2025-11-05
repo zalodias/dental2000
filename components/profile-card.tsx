6afe1e1a4ecbcf4b9c23ae5c2a51ec414b88fc2c
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-05 20:07:15 +0000

    ✨ Add ProfileCard component

---
+++ components/profile-card.tsx
@@ -0,0 +1,21 @@
+import { mergeTailwindClassNames as cn } from '@/utils/tailwind';
+
+interface ProfileCardProps {
+  name: string;
+  title: string;
+  className?: string;
+}
+
+export function ProfileCard({ name, title, className }: ProfileCardProps) {
+  return (
+    <div className={cn('flex flex-col gap-4', className)}>
+      <div className="bg-background-neutral-subtle aspect-1/1 w-full" />
+      <div className="flex flex-col gap-1">
+        <div className="text-title-medium font-medium">{name}</div>
+        <div className="text-body-large text-foreground-neutral-subtle">
+          {title}
+        </div>
+      </div>
+    </div>
+  );
+}
