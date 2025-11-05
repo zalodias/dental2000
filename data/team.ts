5fa77ceaf1d62126f34cf467be598fc8166e3b59
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-05 20:04:45 +0000

    ✨ Add team member interface and initial team data

---
+++ data/team.ts
@@ -0,0 +1,27 @@
+export interface Member {
+  name: string;
+  title: string;
+}
+
+export const team: Member[] = [
+  {
+    name: 'Mário Marques',
+    title: 'Médico Dentista',
+  },
+  {
+    name: 'Francisco Miguel Sanches',
+    title: 'Médico Dentista',
+  },
+  {
+    name: 'João Gonçalo',
+    title: 'Médico Dentista',
+  },
+  {
+    name: 'Ana Cristina Sanches',
+    title: 'Diretora Clínica',
+  },
+  {
+    name: 'Francisco Sanches',
+    title: 'Diretor Clínico',
+  },
+];
