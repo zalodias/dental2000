7f12ba30db521cb5246c1ccc10319b748f360cf7
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-06 22:11:11 +0000

    ✨ Add intro section to Team page

--- app/equipa/page.tsx
+++ app/equipa/page.tsx
@@ -6,12 +6,17 @@ import { SectionHeader } from '@/sections/section-header';
 export default function Equipa() {
   return (
     <>
-      <Container className="mt-18">
+      <Container className="flex items-center lg:flex-row">
+        <div className="bg-background-neutral-subtle relative -mx-5 aspect-4/3 w-screen object-cover md:mx-0 md:w-full" />
         <SectionHeader
           eyebrow="A nossa equipa"
           title="Profissionais dedicados ao seu bem-estar"
+          subtitle="Na Dental 2000, a nossa equipa é composta por
+profissionais altamente qualificados, comprometidos em proporcionar um atendimento de excelência e em constante atualização nas suas áreas de especialização."
         />
-        <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-10">
+      </Container>
+      <Container>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-x-5 gap-y-10">
           {team.map((member) => (
             <ProfileCard
               key={member.name}
