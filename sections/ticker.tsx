8a5c234229f186a5a737ae740e2e747f85808430
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-21 23:13:28 +0000

    ✨ Add Ticker component

---
+++ sections/ticker.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { CSSProperties, ReactNode } from 'react';
+
+interface TickerProps {
+  items: ReactNode[];
+  duration?: number;
+}
+
+export function Ticker({ items, duration = 20 }: TickerProps) {
+  const ticker = [...items, ...items];
+
+  const animation = {
+    ['--ticker-duration']: `${duration}s`,
+  } as CSSProperties;
+
+  return (
+    <section className="relative w-full">
+      <div className="relative py-10">
+        <div
+          className="animate-ticker flex w-fit gap-5"
+          style={{ ...animation }}
+        >
+          {ticker.map((item, index) => (
+            <div
+              key={index}
+              className="aspect-[3/4] w-[clamp(320px,20vw,400px)]"
+              aria-hidden={index >= items.length}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
