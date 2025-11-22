3b2a724797fce6e516403407f87e404187771dcc
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-11-22 22:30:55 +0000

    ✨ Add contacts data

---
+++ data/contacts.ts
@@ -0,0 +1,18 @@
+export interface Contact {
+  name: string;
+  phone: string;
+  address: string;
+}
+
+export const contacts: Contact[] = [
+  {
+    name: 'Coimbra',
+    phone: '(+351) 922 124 622',
+    address: 'R. João de Ruão 5, 3000-153 Coimbra',
+  },
+  {
+    name: 'Sertã',
+    phone: '(+351) 922 133 517',
+    address: 'Rua Vila de Rei, Nº66 R/Ch, 6100-707 Sertã',
+  },
+];
