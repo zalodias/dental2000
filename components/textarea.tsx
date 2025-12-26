91e4b6dfe8c82e68bf1535e888e2b2e7fae8cac0
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-12-26 11:38:12 +0000

    💄 Prefer canonical Tailwind classes

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 export function Textarea({ className, ...props }: TextareaProps) {
   return (
     <textarea
-      className="border-border-neutral-default placeholder:text-foreground-neutral-faded text-title-small focus:border-border-neutral-strong [field-sizing:content] overflow-clip border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
+      className="border-border-neutral-default placeholder:text-foreground-neutral-faded text-title-small focus:border-border-neutral-strong field-sizing-content overflow-clip border-b px-2 py-4 transition-colors duration-200 focus:outline-none"
       onInput={(e) => {
         e.currentTarget.style.height = '';
         e.currentTarget.style.height = e.currentTarget.scrollHeight + 'px';
