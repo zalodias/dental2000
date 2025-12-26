0354a6dd893374da5dd984b3b191a8f40db4d37a
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-12-26 11:38:12 +0000

    💄 Prefer canonical Tailwind classes

--- components/profile-card.tsx
+++ components/profile-card.tsx
@@ -9,7 +9,7 @@ interface ProfileCardProps {
 export function ProfileCard({ name, title, className }: ProfileCardProps) {
   return (
     <div className={cn('flex flex-col gap-4', className)}>
-      <div className="bg-background-neutral-subtle aspect-1/1 w-full" />
+      <div className="bg-background-neutral-subtle aspect-square w-full" />
       <div className="flex flex-col gap-1">
         <div className="text-title-medium font-medium">{name}</div>
         <div className="text-body-large text-foreground-neutral-subtle">
